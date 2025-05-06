c69b7ef5030afac1cd06c93e3edea104b2a10b31
Tecsup <dengoku727@gmail.com>
2025-05-06 11:19:34 -0500

    Labo8

---
+++ Assets/Scritps/GameEvents/GameEventListener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Events;
+using UnityEngine;
+using Assets.Scripts.GameEvents;
+
+namespace Assets.Scritps.GameEvents
+{
+    public class GameEventListener : MonoBehaviour
+    {
+        [SerializeField] private GameEvent gameEvent;
+
+        [SerializeField] private UnityEvent response;
+
+        private void OnEnable()
+        {
+            gameEvent.Register(this);
+        }
+
+        private void OnDisable()
+        {
+            gameEvent.Unregister(this);
+        }
+
+        public void OnEventRaised()
+        {
+            response?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scritps/GameEvents/GameIntEventListener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Events;
+using UnityEngine;
+
+namespace Assets.Scritps.GameEvents
+{
+    public class GameIntEventListener : MonoBehaviour
+    {
+        [SerializeField] private GameIntEvent gameEvent;
+
+        [SerializeField] private UnityEvent<int> response;
+
+        private void OnEnable()
+        {
+            gameEvent.Register(this);
+        }
+
+        private void OnDisable()
+        {
+            gameEvent.Unregister(this);
+        }
+
+        public void OnEventRaised(int value)
+        {
+            response?.Invoke(value);
+        }
+    }
+
+}
